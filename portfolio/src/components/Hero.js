426e8af0f1b6a5a35eec4b82d042435273bb4f7d
Chris <namegpark@naver.com>
2021-08-02 19:10:58 +0900

    img size 조정

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -4,12 +4,12 @@ class HeroSection extends React.Component {
     render() {
         return (
             <div className="flex max-w-screen justify-between px-10 py-20 bg-gray-900 text-white">
-                <div className="flex flex-col justify-center w-1/2">
+                <div className="flex flex-col justify-center items-center w-1/2">
                     <h1 className="text-4xl">Hello. I'm Gabrial Mact</h1>
                     <h2>저는 Full Stack Engineer입니다.</h2>
                 </div>
-                <div className="w-1/2">
-                    <img className="rounded w-full" src="https://t1.daumcdn.net/cfile/tistory/99291E375A87DFF40F?download" alt="hero"></img>
+                <div className="flex justify-center w-1/2">
+                    <img className="rounded h-auto" src="http://placehold.it/500" alt="hero"></img>
                 </div>
             </div>
             
