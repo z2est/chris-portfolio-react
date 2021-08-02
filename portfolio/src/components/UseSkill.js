7643231af9f7fea6a490a3db1345ddf22f239d6b
Chris <namegpark@naver.com>
2021-08-03 03:22:00 +0900

    UseSkill Section box border 수정

--- portfolio/src/components/UseSkill.js
+++ portfolio/src/components/UseSkill.js
@@ -7,14 +7,14 @@ class SkillSection extends React.Component{
 
     render() {
         return (
-            <section className="flex flex-col justify-center">
+            <section className="flex flex-col justify-center px-10">
                 <div className="mt-12 text-center w-auto">
                     <h1 className="text-5xl font-semibold text-gray-900">비즈니스 라인업</h1>
                     <p className="mt-4 text-center text-gray-400">7Connected의 비즈니스 라인업 입니다.</p>
                 </div>
 
-                <div className="flex justify-between item-">
-                    <div className="flex flex-col items-center border-2 border-solid w-1/3 h-auto m-12 rounded">
+                <div className="flex justify-between">
+                    <div className="flex flex-col items-center border border-solid w-1/3 h-auto m-12 rounded shadow-lg">
                         <div className="bg-white-300">
                             <img className="object-fit h-full w-full p-8" src={ MetaBusImg } alt="7Connected"></img>
                         </div>
@@ -22,7 +22,7 @@ class SkillSection extends React.Component{
                         <p className="text-gray-500 text-center my-2.5">메타버스는 7Connected의 가상 환경 플랫폼입니다.</p>
                     </div>
 
-                    <div className="flex flex-col items-center border-2 border-solid w-1/3 h-auto m-12 rounded">
+                    <div className="flex flex-col items-center border border-solid w-1/3 h-auto m-12 rounded shadow-lg">
                         <div className="bg-white-300">
                             <img className="object-fit h-full w-full p-8" src={ BlockChainImg } alt="7Connected"></img>
                         </div>
@@ -30,7 +30,7 @@ class SkillSection extends React.Component{
                         <p className="text-gray-500 text-center my-2.5">메타버스는 7Connected의 가상 환경 플랫폼입니다.</p>
                     </div>
 
-                    <div className="flex flex-col items-center border-2 border-solid w-1/3 h-auto m-12 rounded">
+                    <div className="flex flex-col items-center border border-solid w-1/3 h-auto m-12 rounded shadow-lg">
                         <div className="bg-white-300">
                             <img className="object-fit h-full w-full p-8" src={ ConsultImg } alt="7Connected"></img>
                         </div>
