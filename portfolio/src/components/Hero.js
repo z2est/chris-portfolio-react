04ec9881f13fb33aea2580f81447e5a744517151
ChrisPark <namegpark@naver.com>
2021-08-02 18:22:19 +0900

    HeroSection 레이아웃 작업

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -4,11 +4,13 @@ class HeroSection extends React.Component {
     render() {
         return (
             <div className="flex max-w-screen justify-between px-10 py-20 bg-gray-900 text-white">
-                <div>
-                    <h1>Hello. I'm Gabrial Mact</h1>
+                <div className="flex flex-col justify-center w-1/2">
+                    <h1 className="text-4xl">Hello. I'm Gabrial Mact</h1>
                     <h2>저는 Full Stack Engineer입니다.</h2>
                 </div>
-                <img src="https://t1.daumcdn.net/cfile/tistory/99291E375A87DFF40F?download" alt="hero"></img>
+                <div className="w-1/2">
+                    <img className="rounded w-full" src="https://t1.daumcdn.net/cfile/tistory/99291E375A87DFF40F?download" alt="hero"></img>
+                </div>
             </div>
             
         );
