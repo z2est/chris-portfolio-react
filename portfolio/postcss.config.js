49c7bbfa9245b8f37d164d590a29dc0d0b20b6fc
Chris <namegpark@naver.com>
2021-08-13 01:25:11 +0900

    프론트 수정작업#2

--- portfolio/postcss.config.js
+++ portfolio/postcss.config.js
@@ -1,6 +1,8 @@
 const tailwindcss = require('tailwindcss');
 module.exports = {
   plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
     tailwindcss('./tailwind.js'),
     require('autoprefixer')
   ]
--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import HeroImg from '../images/hero.png';
+import './styles/Hero.scss';
 
 class HeroSection extends React.Component {
     render() {
         return (
-            <div className="flex max-w-screen h-screen justify-between px-10 py-20 bg-gray-900 text-white">
-                <div className="flex flex-col justify-center w-1/2">
-                    <div>
-                        <h1 className="text-5xl mb-4 font-bold">내 손안의 물류플랫폼</h1>
-                        <p className="text-3xl">그랩은 빠르고 안전한 물류 서비스입니다.</p>
+            <div className="container-main flex max-w-screen h-screen justify-between px-10 py-20 bg-gray-900 text-white bg-cover">
+                <div className="flex flex-col justify-center items-center w-full">
+                    <div className="text-center">
+                        <h1 className="text-5xl mb-4 font-lighter">Change is coming like a wave</h1>
+                        <p className="text-2xl">물류를 바꾸는 혁신. Grab</p>
                     </div>
-                    <div>
-                        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">물류 접수</button>
-                        <button>접수 현황</button>
+                    <div className="flex w-80 m-5 justify-between">
+                        <button className="w-2/5 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Google Play</button>
+                        <button className="w-2/5 bg-blue-500 hover:bg-transparent text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">AppStore</button>
                     </div>
-                </div>
-                <div className="flex justify-center items-center w-1/2 pb-40">
-                    <img className="w-4/5 object-fit rounded" src={HeroImg} alt="hero"/>
                 </div>
             </div>
         );
