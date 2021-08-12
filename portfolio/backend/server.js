588f9d3b239c6be67f485ade487e21b1c703e1c2
ChrisPark <namegpark@naver.com>
2021-08-12 20:05:10 +0900

    프론트 수정작업

--- portfolio/backend/server.js
+++ portfolio/backend/server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const DOMParser = require('dom-parser');
 const app = express();
 const bodyParser = require('body-parser');
-const port =process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
-app.use('/api', (req, res)=> res.json({username:'bryan'}));
+
+app.use('/api', (req, res)=> {
+    //const body = req.body;
+    //const instaUrl = body.url;
+    const instaUrl = "https://www.instagram.com/p/CSZeEhqpGthNu76kfXbZM9X03OJqFrAvnLIR3M0/?utm_medium=copy_link";
+    let photoUrl = "";
+
+    fetch(instaUrl).then((res) => {
+        return res.text();
+    }).then((html) => {
+        // Parse PhotoUrl from instagram
+        //console.log(html);
+        //document.inputEncoding
+        const domparser = new DOMParser();
+        let doc = domparser.parseFromString(html);
+
+        const links = doc.getElementsByTagName("img");
+        console.log(links.length);
+        links.forEach(elem => {
+            console.log(elem.getAttribute("src"));
+        })
+
+    });
+
+    res.json({username:'bryan'})
+});
 
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -5,12 +5,15 @@ class HeroSection extends React.Component {
     render() {
         return (
             <div className="flex max-w-screen h-screen justify-between px-10 py-20 bg-gray-900 text-white">
-                <div className="flex justify-center items-center w-1/2">
+                <div className="flex flex-col justify-center w-1/2">
                     <div>
-                        <h1 className="text-5xl mb-4 font-bold">All The Best, 7Connected.</h1>
-                        <p>우리는 7Connected 입니다.</p>
+                        <h1 className="text-5xl mb-4 font-bold">내 손안의 물류플랫폼</h1>
+                        <p className="text-3xl">그랩은 빠르고 안전한 물류 서비스입니다.</p>
                     </div>
-
+                    <div>
+                        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">물류 접수</button>
+                        <button>접수 현황</button>
+                    </div>
                 </div>
                 <div className="flex justify-center items-center w-1/2 pb-40">
                     <img className="w-4/5 object-fit rounded" src={HeroImg} alt="hero"/>
